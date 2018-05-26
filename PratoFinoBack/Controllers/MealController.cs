1b252f2b7b61022db530c32d47380ada691bca43
ArthurFR <contato.arthurfr@gmail.com>
2018-05-26 00:58:27 -0300

    REST Api

--- PratoFinoBack/Controllers/MealController.cs
+++ PratoFinoBack/Controllers/MealController.cs
@@ -1,10 +1,84 @@
 using Microsoft.AspNetCore.Mvc;
+using PratoFinoBack.Data;
+using PratoFinoBack.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PratoFinoBack.Controllers
 {
     [Route("api/[controller]")]
     public class MealController : Controller
     {
+        private readonly PratoFinoDbContext context;
 
+        public MealController(PratoFinoDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Meal> GetAll()
+        {
+            return context.Meals.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var meal = context.Meals.Find(id);
+            if(meal == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(meal);
+        }
+
+        [HttpPost]
+        public void Insert([FromBody]Meal meal)
+        {
+            if (meal == null)
+            {
+                return;
+            }
+
+            context.Add(meal);
+            context.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]Meal item)
+        {
+            if(item == null || item.Id != id )
+            {
+                return BadRequest();
+            }
+
+            var meal = context.Meals.Find(id);
+            if(meal == null)
+            {
+                return NotFound();
+            }
+
+            meal.Name = item.Name;
+            meal.Restaurant = item.Restaurant;
+            context.Meals.Update(meal);
+            context.SaveChanges();
+            return NoContent(); 
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete (int id)
+        {
+            var meal = context.Meals.Find(id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            context.Meals.Remove(meal);
+            context.SaveChanges();
+            return NoContent();
+        }
     }
 }
--- PratoFinoBack/Controllers/RestaurantController.cs
+++ PratoFinoBack/Controllers/RestaurantController.cs
@@ -1,10 +1,84 @@
 using Microsoft.AspNetCore.Mvc;
+using PratoFinoBack.Data;
+using PratoFinoBack.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace PratoFinoBack.Controllers
 {
     [Route("api/[controller]")]
     public class RestaurantController : Controller
     {
-        
+        private readonly PratoFinoDbContext context;
+        public RestaurantController(PratoFinoDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Restaurant> GetAll()
+        {
+            return context.Restaurants.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetRestaurant")]
+        public IActionResult GetById(int id)
+        {
+            var restaurant = context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(restaurant);
+        }
+
+        [HttpPost]
+        public void Insert([FromBody] Restaurant restaurant)
+        {
+            if(restaurant == null)
+            {
+                return;
+            }
+
+            context.Restaurants.Add(restaurant);
+            context.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Restaurant item)
+        {
+            if(item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var restaurant = context.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            restaurant.Name = item.Name;
+            
+            context.Restaurants.Update(restaurant);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var restaurant = context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return NotFound();
+            }
+
+            context.Restaurants.Remove(restaurant);
+            context.SaveChanges();
+            return NoContent();
+        }
     }
 }
